b2846dd690cbdedfcbb48ccaea5429e1a6289b3e
nam tran trong <nam.tran-trong@ubisoft.com>
2024-10-03 12:50:17 +0700

    implement input actions

---
+++ Assets/Scripts/GameSystems/Inputs/InputWrapper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine.InputSystem;
+
+namespace Gameplay
+{
+    public class InputWrapper
+    {
+        public bool IsPressed { get; private set; }
+        public bool IsReleased => !IsPressed;
+
+        public readonly InputAction m_inputAction;
+        public readonly EPlayerInputType m_inputType;
+        private readonly Action<InputAction.CallbackContext> m_inputActionOnperformed;
+    }
+}
---
+++ Assets/Scripts/GameSystems/Utils/PlayerInputBindings.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using UnityEngine.InputSystem;
+
+namespace Gameplay
+{
+    public enum EPlayerInputType
+    {
+        Movement = 0,
+        Jump = 1,
+        Attack = 2,
+    }
+    
+    public enum EPlayerInputControlSchemes
+    {
+        Pad,
+        Keyboard,
+    }
+    
+    public class PlayerInputBindings
+    {
+        public PadInput PlayerInput { get; } = new PadInput();
+        public Dictionary<EPlayerInputType, InputAction> PlayerInputTypeMap { get; } 
+        public Dictionary<InputAction, EPlayerInputType> PlayerInputTypeMapReverse { get; }
+
+        public PlayerInputBindings()
+        {
+            PlayerInputTypeMap = new Dictionary<EPlayerInputType, InputAction>();
+            PlayerInputTypeMapReverse = new Dictionary<InputAction, EPlayerInputType>();
+            
+            // Movement
+            PlayerInputTypeMap.Add(EPlayerInputType.Movement, PlayerInput.Gameplay.Movement);
+            PlayerInputTypeMapReverse.Add(PlayerInput.Gameplay.Movement, EPlayerInputType.Movement);
+            // Jump
+            PlayerInputTypeMap.Add(EPlayerInputType.Jump, PlayerInput.Gameplay.Jump);
+            PlayerInputTypeMapReverse.Add(PlayerInput.Gameplay.Jump, EPlayerInputType.Jump);
+            // Attack
+            PlayerInputTypeMap.Add(EPlayerInputType.Attack, PlayerInput.Gameplay.Attack);
+            PlayerInputTypeMapReverse.Add(PlayerInput.Gameplay.Attack, EPlayerInputType.Attack);
+        }
+    }
+}
